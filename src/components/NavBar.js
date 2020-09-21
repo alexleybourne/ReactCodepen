dfe122abc321c55e3eb1e0b0446d74352c4e30e2
alexleybourne <alexleybourne1@gmail.com>
2020-09-21 23:27:12 +1000

    Added nav logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import logo from '../assets/CodeCoopLogo.png'
+
 export default function NavBar() {
     return (
         <div className="nav">
-            <div className="left"></div>
+            <div className="left">
+                <img src={logo} alt="logo"/>
+            </div>
             <div className="right"></div>
         </div>
     )
